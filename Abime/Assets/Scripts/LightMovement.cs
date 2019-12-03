9b935373636d6be06a6827e0c7fd53de0eec0580
Loic Lachiver <llachiver@emi.u-bordeaux1.fr>
2019-12-03 19:38:56 +0100

    lighting test + basic movement

---
+++ Abime/Assets/Scripts/LightMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour {
+
+	public int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis("Horizontal") * speed;
+
+		Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 180;
+		transform.rotation = Quaternion.Euler(angle,-90,0);
+
+		transform.position += new Vector3 (translation, 0, 0);
+
+	}
+}
