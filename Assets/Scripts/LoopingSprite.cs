65eaaa7d1dc1b911eaf21cb70ccfcde659dcd91b
RhenaudTheLukark <rhen33390@gmail.com>
2019-12-03 19:38:25 +0100

    Added parallax + autoscrolling

---
+++ Assets/Scripts/LoopingSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LoopingSprite : MonoBehaviour {
+    private RectTransform rect;
+
+    public int startY = 0;
+    public int loopVal = 100;
+
+    // Start is called before the first frame update
+    void Start() {
+        rect = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (rect.position.y < startY)
+            rect.position += Vector3.up * loopVal;
+        if (rect.position.y > startY + loopVal)
+            rect.position -= Vector3.up * loopVal;
+    }
+}
--- Assets/Scripts/Vertical/PlayerControllerVertical.cs
+++ Assets/Scripts/Vertical/PlayerControllerVertical.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerVertical : MonoBehaviour {
-    Transform tf;
-    public int speed = 3;
+    RectTransform tf;
+    Transform mainCanvas;
+    Transform bg;
+    public int xSpeed = 3;
+    public float ySpeed = 1.5f;
+    public float yCSpeed = 2f;
 
     // Start is called before the first frame update
     void Start() {
-        tf = gameObject.GetComponent<Transform>();
+        tf = gameObject.GetComponent<RectTransform>();
+        mainCanvas = GameObject.Find("LevelParent").transform;
+        bg = GameObject.Find("bg").transform;
     }
 
     // Update is called once per frame
     void Update() {
-        if (GlobalControls.input.Left > 0)  tf.position += Vector3.left  * speed;
-        if (GlobalControls.input.Right > 0) tf.position += Vector3.right * speed;
-        if (GlobalControls.input.Up > 0)    tf.position += Vector3.up    * speed;
-        if (GlobalControls.input.Down > 0)  tf.position += Vector3.down  * speed;
+        mainCanvas.position += Vector3.up * yCSpeed;
+        bg.position += Vector3.up * yCSpeed / 2;
+
+        float parallaxYSpeed = ySpeed / 2;
+        if (GlobalControls.input.Up > 0) {
+            mainCanvas.position += Vector3.down * ySpeed;
+            bg.position += Vector3.down * ySpeed / 2;
+        }
+        if (GlobalControls.input.Down > 0) {
+            mainCanvas.position += Vector3.up * ySpeed;
+            bg.position += Vector3.up * ySpeed / 2;
+        }
+
+        if (GlobalControls.input.Left > 0)  tf.position += Vector3.left  * xSpeed;
+        if (GlobalControls.input.Right > 0) tf.position += Vector3.right * xSpeed;
+
+        Debug.Log(tf.position);
+
+        if (tf.localPosition.x < 120) tf.localPosition = new Vector3(120, tf.localPosition.y, tf.localPosition.z);
+        if (tf.localPosition.x > 520) tf.localPosition = new Vector3(520, tf.localPosition.y, tf.localPosition.z);
     }
 }
