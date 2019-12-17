4a19790e4c1b4dfd1d840626ab97a1dfadc7e958
ReportHarambe <fouray.nino@gmail.com>
2019-12-17 19:11:49 +0100

    Ajout de la scene horizontale, modification de la version à 2019.2.16

---
+++ Assets/Scripts/Horizontal/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMovement = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Move
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
